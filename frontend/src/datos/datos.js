60d0695abba53591768ae8e31175e352c06071d6
Alejandro Di Battista <alejandrodibattista@gmail.com>
2024-11-06 10:58:49 -0300

    Elegir carga los datos dinamicamente

--- frontend/src/datos/datos.js
+++ frontend/src/datos/datos.js
@@ -1,8 +1,10 @@
-import productos from './productos.json';
+import productos  from './productos.json';
 import categorias from './categorias.json';
 
 function traerProductos(id) {
-    return productos.filter(producto => producto.categoria === id);
+    console.log(">> traerProductos", id);
+    console.log(productos.filter(producto => producto.categoria_id == id));
+    return productos.filter(producto => producto.categoria_id == id);
 }
 
 function traerCaterorias() {
