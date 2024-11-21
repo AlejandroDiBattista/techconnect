3b8a7fcde8acdbaa3cf7ac45dc8debc0d7a2b87c
Joel Corbalán <141983168+Joel210211@users.noreply.github.com>
2024-11-20 21:02:07 -0300

    .
    .

--- frontend/src/components/Logo.jsx
+++ frontend/src/components/Logo.jsx
@@ -4,7 +4,7 @@ import { Text } from '@radix-ui/themes';
 const Logo = () => {
     const navigate = useNavigate();    
     return <>
-        <Text size="6"  onClick={() => navigate('/')}>Tecno Connet</Text>
+        <Text size="6"  onClick={() => navigate('/')}>TechConnect</Text>
     </>
 };
 
